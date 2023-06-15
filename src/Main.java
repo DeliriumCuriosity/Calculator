a324dcc640d8a430cb851648d2b393c229914b13
DeliriumCuriosity <100794261+DeliriumCuriosity@users.noreply.github.com>
2023-06-15 20:07:05 +0300

    Update Main.java
    Исправление для Kata

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите арифметическое выражение: ");
         String input = scanner.nextLine();
@@ -10,30 +10,26 @@ public class Main {
         System.out.println("Результат: " + result);
     }
 
-    public static String calc(String input) {
-        try {
-            String[] tokens = input.split(" ");
-            int num1 = Integer.parseInt(tokens[0]);
-            int num2 = Integer.parseInt(tokens[2]);
-            String operator = tokens[1];
+    public static String calc(String input) throws IOException {
+        String[] tokens = input.split(" ");
+        int num1 = Integer.parseInt(tokens[0]);
+        int num2 = Integer.parseInt(tokens[2]);
+        String operator = tokens[1];
 
-            int result;
-            if (num1 >= 1 && num1 < 11 && num2 >= 1 && num2 < 11) {
-                switch (operator) {
-                    case "+" -> result = num1 + num2;
-                    case "-" -> result = num1 - num2;
-                    case "*" -> result = num1 * num2;
-                    case "/" -> result = num1 / num2;
-                    default -> {
-                        return "Неподдерживаемая операция";
-                    }
+        int result;
+        if (num1 >= 1 && num1 < 11 && num2 >= 1 && num2 < 11) {
+            switch (operator) {
+                case "+" -> result = num1 + num2;
+                case "-" -> result = num1 - num2;
+                case "*" -> result = num1 * num2;
+                case "/" -> result = num1 / num2;
+                default -> {
+                    throw new IOException();
                 }
-            } else {
-                throw new IOException();
             }
-            return String.valueOf(result);
-        } catch (Exception e) {
-            return "Ошибка! Проверьте правильность написания арифметического выражения!";
+        } else {
+            throw new IOException();
         }
+        return String.valueOf(result);
     }
 }
