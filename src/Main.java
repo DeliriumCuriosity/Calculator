fddb26077e3ec0a2acfd142bc5f7291eecc85030
DeliriumCuriosity <100794261+DeliriumCuriosity@users.noreply.github.com>
2023-06-16 22:13:24 +0300

    Update Main.java
    Добавлено ограничение по количеству используемых элементов в операции.

--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,31 @@ public class Main {
 
     public static String calc(String input) throws IOException {
         String[] tokens = input.split(" ");
-        int num1 = Integer.parseInt(tokens[0]);
-        int num2 = Integer.parseInt(tokens[2]);
-        String operator = tokens[1];
+        int sumOfTokens = tokens.length;
+        if (sumOfTokens == 3) {
 
-        int result;
-        if (num1 >= 1 && num1 < 11 && num2 >= 1 && num2 < 11) {
-            switch (operator) {
-                case "+" -> result = num1 + num2;
-                case "-" -> result = num1 - num2;
-                case "*" -> result = num1 * num2;
-                case "/" -> result = num1 / num2;
-                default -> {
-                    throw new IOException();
+
+            int num1 = Integer.parseInt(tokens[0]);
+            int num2 = Integer.parseInt(tokens[2]);
+            String operator = tokens[1];
+
+            int result;
+            if (num1 >= 1 && num1 < 11 && num2 >= 1 && num2 < 11) {
+                switch (operator) {
+                    case "+" -> result = num1 + num2;
+                    case "-" -> result = num1 - num2;
+                    case "*" -> result = num1 * num2;
+                    case "/" -> result = num1 / num2;
+                    default -> {
+                        throw new IOException();
+                    }
                 }
+            } else {
+                throw new IOException();
             }
+            return String.valueOf(result);
         } else {
             throw new IOException();
         }
-        return String.valueOf(result);
     }
 }
